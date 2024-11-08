be4886641f73b4ac5866d90c12575c3e776676da
longuigon <longuigon+git@gmail.com>
2024-11-08 09:16:52 -0300

    creado alumnos

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Activities from './Pages/Activities';
+import Alumnos from './Pages/Alumnos';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/Login"/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/activities" element={<Activities />} />
+        <Route path="/alumnos" element={<Alumnos />} />
       </Routes>
     </Router>
   );
---
+++ vite-project/src/Pages/Alumnos.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../Styles/Alumnos.module.css';
+
+const Alumnos = () => {
+
+    return (
+        <div>
+            <h2 className={styles.pageHeader}>Alumnos</h2>
+        </div>
+    );
+}
+
+export default Alumnos;
