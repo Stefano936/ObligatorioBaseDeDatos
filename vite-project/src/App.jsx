30515f6c22ba0c1926bb0d04278ac650d98c67c3
Stefano <stefano.francolino@correo.ucu.edu.uy>
2024-11-18 15:23:47 -0300

    ..

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
-import Register from './Pages/Register';
 import Activities from './Pages/Activities';
 import Alumnos from './Pages/Alumnos';
 import Instructores from './Pages/Instructores';
@@ -17,7 +16,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/Login"/>}/> // Redirect to Login
         <Route path="/login" element={<Login />} /> // Login page
-        <Route path="/register" element={<Register />} /> // Register page
         <Route path="/inscripciones" element={<Activities />} /> // Activities page
         <Route path="/actividades" element={<Actividades />} /> // Accede solo admin para modidficar esto
         <Route path="/alumnos" element={<Alumnos />} /> // Accede solo admin para modificar eso
